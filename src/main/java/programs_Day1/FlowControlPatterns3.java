19ead2529d8401591afc416ba779baaba9bdcbd9
Maria Sanchez <msanchez157@gmail.com>
2021-03-07 11:26:48 -0500

    Flow Control Patterns: Create Four Patterns

--- src/main/java/programs_Day1/FlowControlPatterns3.java
+++ src/main/java/programs_Day1/FlowControlPatterns3.java
@@ -2,13 +2,21 @@
  * (Display four patterns using loops)
  *  Use nested loops that display the following patterns in four separate programs:
  *
- *            Pattern A           Pattern B          Pattern C            Pattern D
- *            1       	         1 2 3 4 5 6                 1 	        1 2 3 4 5 6
- *            1 2     	         1 2 3 4 5                 2 1   	      1 2 3 4 5
- *            1 2 3   	         1 2 3 4                 3 2 1              1 2 3 4
- *            1 2 3 4 	         1 2 3                 4 3 2 1                1 2 3
- *            1 2 3 4 5 	     1 2 	             5 4 3 2 1 	    	        1 2
- *            1 2 3 4 5 6        1                 6 5 4 3 2 1 	                  1
+ *            Pattern A          Pattern B
+ *            1                  1 2 3 4 5 6
+ *            1 2                1 2 3 4 5
+ *            1 2 3              1 2 3 4
+ *            1 2 3 4            1 2 3
+ *            1 2 3 4 5          1 2
+ *            1 2 3 4 5 6        1
+ *
+ *            Pattern C         Pattern D
+ *            1                 1 2 3 4 5 6
+ *            2 1                 1 2 3 4 5
+ *            3 2 1                 1 2 3 4
+ *            4 3 2 1                 1 2 3
+ *            5 4 3 2 1                 1 2
+ *            6 5 4 3 2 1                 1
  */
 
 package programs_Day1;
